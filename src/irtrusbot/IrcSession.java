ab4c20319ade8559bb3b2efa43ed29a3d6b5bdf8
crashdemons <crashenator+gh@gmail.com>
2015-03-17 14:32:59 -0500

    removing template text

--- src/irtrusbot/IrcSession.java
+++ src/irtrusbot/IrcSession.java
@@ -1,18 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package irtrusbot;
-
-
-import java.io.*;
-import java.net.*;
 
 /**
  *
  * @author crashdemons <crashdemons -at- github.com>
  */
+
+import java.io.*;
+import java.net.*;
+
+
 public class IrcSession {
     public IrcOrigin account;
     String rname="";
