db8ed0fb4e809fcf94ecc245d4f3a3d9eee17728
crashdemons <crashenator+gh@gmail.com>
2015-03-17 19:14:46 -0500

    indention

--- src/irtrusbot/IrtrusBot.java
+++ src/irtrusbot/IrtrusBot.java
@@ -25,43 +25,41 @@ public class IrtrusBot
 
     private static void startIRC() throws Exception
     {
-            String server = "irc.freenode.net", nick = "IrtrusBot2", user = "IrtrusBot2", channel = "#irtrusbot,#cicada";
+        String server = "irc.freenode.net", nick = "IrtrusBot2", user = "IrtrusBot2", channel = "#irtrusbot,#cicada";
 
-            IrcSession irc=new IrcSession();
-            irc.setAccountDetails(nick,user,"Java Hacks Bot","");//no password=""
-            irc.setConnectionDetails(server, 6667);
+        IrcSession irc=new IrcSession();
+        irc.setAccountDetails(nick,user,"Java Hacks Bot","");//no password=""
+        irc.setConnectionDetails(server, 6667);
 
-            irc.connect();//establish TCP connection.
-            irc.loginwait();//send NICK+USER and wait for 001-004 or failure
-            irc.identify();//send nickserv identify if necessary
-            irc.sendMessage(irc.account,nick,"++userinit");//this is sent so that we can obtain our full hostmask/identd state easily - useful for post length calculations later.
-            irc.sendRawLine("JOIN "+channel);
+        irc.connect();//establish TCP connection.
+        irc.loginwait();//send NICK+USER and wait for 001-004 or failure
+        irc.identify();//send nickserv identify if necessary
+        irc.sendMessage(irc.account,nick,"++userinit");//this is sent so that we can obtain our full hostmask/identd state easily - useful for post length calculations later.
+        irc.sendRawLine("JOIN "+channel);
 
-            IrcCommand ic;
-            while((ic=irc.readCommand())!=null && irc.isConnected()){
-                    //System.out.println("Entered loop...");
-                    //debugObject(ic);
+        IrcCommand ic;
+        while((ic=irc.readCommand())!=null && irc.isConnected()){
+            //System.out.println("Entered loop...");
+            //debugObject(ic);
 
-                    if(ic.type.equals("PRIVMSG")){
-                        IrcMessage im=new IrcMessage(ic);
+            if(ic.type.equals("PRIVMSG")){
+                IrcMessage im=new IrcMessage(ic);
 
-                        boolean fromAdmin=(im.from.nick.equals("crashdemons") || im.from.nick.equals("Surtri"));
-                        boolean fromSelf =im.from.nick.equals(nick);
-
-
-                        if(im.text.startsWith("++userinit") && fromSelf){ irc.account=im.from; }//this is our test PM to ourself that grabs the username/hostname for us.
-
-                        if(im.text.startsWith("+quit") && fromAdmin){ irc.disconnect(); break; }
-                        if(im.text.startsWith("+rr")){
-                            String text=russianRouletteFire();
-                            irc.sendReply(im, text, false);//reply to the user in whatever tab he's in (PM or channel)
-                        }
-                    }
-                    if(ic.type.equals("PING")) irc.sendRawLine("PONG :"+ic.parameters.get(0));//pong back with the first parameter of the PING.
+                boolean fromAdmin=(im.from.nick.equals("crashdemons") || im.from.nick.equals("Surtri"));
+                boolean fromSelf =im.from.nick.equals(nick);
 
 
+                if(im.text.startsWith("++userinit") && fromSelf){ irc.account=im.from; }//this is our test PM to ourself that grabs the username/hostname for us.
+
+                if(im.text.startsWith("+quit") && fromAdmin){ irc.disconnect(); break; }
+                if(im.text.startsWith("+rr")){
+                    String text=russianRouletteFire();
+                    irc.sendReply(im, text, false);//reply to the user in whatever tab he's in (PM or channel)
+                }
             }
-            //socket has disconnected.
+            if(ic.type.equals("PING")) irc.sendRawLine("PONG :"+ic.parameters.get(0));//pong back with the first parameter of the PING.
+        }
+        //socket has disconnected.
     }
 
     private static void russianRouletteSpin(){//randomize the bullet placement for a round of russian roulette.
